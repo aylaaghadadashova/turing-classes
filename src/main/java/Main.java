e7c0cd1336b4d4f9f3e96c4837144cfb92cebb54
Ayla <ayla.aghadadashova.std@bhos.edu.az>
2025-06-14 20:38:52 +0400

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+       /*
         double a =5;
         long b =7;
         int c = 3;
@@ -7,5 +10,13 @@ public class Main {
 
         System.out.printf("result: %d, %s, %.2f\n", 7, "salam", 5.58764);
         System.out.printf("result: %.0f\n",((a++) + (--b) - (a++) + (a+=3) + (++c)));
+        */
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a>b? "faxri" : a<b? "tural" : "murad");
+
+
     }
 }
