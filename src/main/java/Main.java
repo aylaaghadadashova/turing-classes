abb514a7f63b4c113adcf3ef59514d46ea8d91f2
Ayla <ayla.aghadadashova.std@bhos.edu.az>
2025-06-14 20:03:50 +0400

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int a=7;
-        int b=8;
-        System.out.println(a+ b);
+        double a =5;
+        long b =7;
+        int c = 3;
+
+
+        System.out.printf("result: %d, %s, %.2f\n", 7, "salam", 5.58764);
+        System.out.println(((a++) + (--b) - (a++) + (a+=3) + (++c)));
     }
 }
